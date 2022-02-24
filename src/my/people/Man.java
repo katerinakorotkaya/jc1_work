abe0e39c3e95a55cfeae8ca5451c84bfb59ce9df
katerinakorotkaya <katerinakorotkaya1864@gmail.com>
2022-02-24 19:57:15 +0300

    add types

--- src/my/people/Man.java
+++ src/my/people/Man.java
@@ -2,5 +2,21 @@ package my.people;
 
 public class Man {
     int age = 18;
+    String firstName = "Vasia";
+    char nameLetter = 'V';
+    int binAge = 0b010101;
+    int octAge = 025;
+    int hexAge = 0xAB;
+    long cell = 1_000_000L;
+    float weight = 1.11F;
+    double height = 0.34D;
+    boolean isSingle = false; //true
+    short s1 = 123;
+    byte b1 = 123;
 
+    public int getAge() {
+        int newAge = 25;
+        age = newAge;
+        return age;
+    }
 }
