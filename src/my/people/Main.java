8aa765870d4d6e71b3a0b44ff9a56b3081e7133a
katerinakorotkaya <katerinakorotkaya1864@gmail.com>
2022-02-24 21:28:12 +0300

    add constructor

--- src/my/people/Main.java
+++ src/my/people/Main.java
@@ -1,8 +1,31 @@
 package my.people;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        Man myMan = new Man();
-        myMan.getAge();
+    static List list = new ArrayList();
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Man myMan2;
+        Man myMan1 = new Man(35);
+        myMan1.getAge();
+        System.out.println(myMan1.getAge());
+
+        myMan2 = myMan1;
+        System.out.println(myMan2.getAge());
+
+        myMan2.age = 25;
+        System.out.println(myMan2.getAge());
+        System.out.println(myMan1.getAge());
+        Thread.sleep(10_000);
+        for (int i = 0; i < 100_000_000; i++) {
+            list.add(new Man(i));
+            // Thread.sleep(1);
+        }
     }
 }
+//A b = new C(); // 2 класса (А для указания типа переменной b; рисвоение b на объект класса С)
+//class С extends A {} //subclass C
+// cladd A {} //super class A
