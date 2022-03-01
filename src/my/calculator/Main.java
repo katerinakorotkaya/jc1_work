5f84a74e6641c04eebc05d5dbb5a9f9bb7c2e0f2
katerinakorotkaya <katerinakorotkaya1864@gmail.com>
2022-03-01 20:51:58 +0300

    add switch operator, IgnoreCase and System.exit

--- src/my/calculator/Main.java
+++ src/my/calculator/Main.java
@@ -4,20 +4,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Please enter first number: ");
-        Scanner scanner = new Scanner(System.in);
-        int firstNumber = scanner.nextInt();
+        while (true) {
+            System.out.println("Please enter first number: ");
+            Scanner scanner = new Scanner(System.in);
+            int firstNumber = scanner.nextInt();
 
-        System.out.println("Enter operation: ");
-        String operation = scanner.next();
+            System.out.println("Enter operation: ");
+            String operation = scanner.next();
 
-        System.out.println("Enter second number: ");
-        int secondNumber = scanner.nextInt();
+            System.out.println("Enter second number: ");
+            int secondNumber = scanner.nextInt();
 
-        BasicCalculator basicCalculator = new BasicCalculator();
-        final double result = basicCalculator.calculate(firstNumber, secondNumber, operation.charAt(0));
-        System.out.println("Result of " + firstNumber + operation + secondNumber + " = " + result);
+            BasicCalculator basicCalculator = new BasicCalculator();
+            final double result = basicCalculator.calculate(firstNumber, secondNumber, operation.charAt(0));
+            System.out.println("Result of " + firstNumber + operation.charAt(0) + secondNumber + " = " + result);
+
+            System.out.print("Please enter [C] to continue or [E] to exit");
+            String command = scanner.next();
+            if (command.equalsIgnoreCase("E")){
+                System.exit(0);
+            }
+        }
     }
 }
-
 
