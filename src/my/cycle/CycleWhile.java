781663bf9a3162972117edcd4c3ba7cc94cfb82a
katerinakorotkaya <katerinakorotkaya1864@gmail.com>
2022-03-03 21:26:55 +0300

    add cycle While

---
+++ src/my/cycle/CycleWhile.java
@@ -0,0 +1,15 @@
+package my.cycle;
+
+public class CycleWhile {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 0;
+        while (i < 20) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+            i++;
+        }
+        System.out.println("sum = " + sum);
+    }
+}
