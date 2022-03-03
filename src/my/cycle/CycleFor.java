0f5aa96356ac2ab99e80e50300f18acddecec706
katerinakorotkaya <katerinakorotkaya1864@gmail.com>
2022-03-03 21:26:31 +0300

    add cycle For

---
+++ src/my/cycle/CycleFor.java
@@ -0,0 +1,9 @@
+package my.cycle;
+
+public class CycleFor {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
