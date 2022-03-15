998bb46cba4bea2b86b0b263cfb3bd051e5f03e0
katerinakorotkaya <katerinakorotkaya1864@gmail.com>
2022-03-15 19:03:45 +0300

    add array and sort

---
+++ src/my/array/ArraySort.java
@@ -0,0 +1,12 @@
+package my.array;
+
+import java.util.Arrays;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        int[] testarray = {1, 4, 7, 3, 4, 5, 9, 3, 4};
+        System.out.println(Arrays.toString(testarray));
+        Arrays.sort(testarray);
+        System.out.println(Arrays.toString(testarray));
+    }
+}
