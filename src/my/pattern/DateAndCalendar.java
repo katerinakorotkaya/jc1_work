302dfabeb0ff7b14089f741f9988c43535490750
katerinakorotkaya <katerinakorotkaya1864@gmail.com>
2022-03-17 18:46:07 +0300

    add class Date and class Calendar

---
+++ src/my/pattern/DateAndCalendar.java
@@ -0,0 +1,14 @@
+package my.pattern;
+
+import java.util.Date;
+import java.util.Calendar;
+
+public class DateAndCalendar {
+    public static void main(String[] args) {
+        Date currentDate = new Date(0);
+        System.out.println("Current Date: " + currentDate);
+
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+    }
+}
